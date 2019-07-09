21d65f27b9290d9c2e4ba4a26efa119dd4573d0b
Juan <jcc.utn@gmail.com>
2019-07-09 09:56:16 +0200

    Final commit after winning the hackathon :)

--- contracts/TrustReviewToken.sol
+++ contracts/TrustReviewToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
+
 /** @author Juan Castellon
     @title Token contract to be used in TrustReview Platform.
     @dev Token is a contract that extends OpenZeppelin audited token contracts
@@ -13,17 +14,10 @@ contract TrustReviewToken is ERC20, ERC20Detailed {
     uint8 private constant NO_DECIMALS = 0;
 
     /** State variables */
-    address private tokenFactory;
     address private company;
 
     /** Constructors */
-    /** @dev Default constructor to create a token on the Platform. It uses 
-            ERC20Detailed, ERC20 and Owned constructors to set state variables. 
-        @param aCompany address of the company that requests this token creation.
-        @param tokenName string with the name of the token.
-        @param tokenSymbol string with a 3 chars (aprox) token symbol.
-        @param initialSupply uint256 with initial total supply for this token.
-    */
+
     constructor(
         address aCompany,
         string memory tokenName,
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,9 @@
 const Migrations = artifacts.require("Migrations");
+const TrustReviewToken = artifacts.require("TrustReviewToken");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
   deployer.deploy(Migrations);
+  deployer.deploy(TrustReviewToken, accounts[0], "TrustReviewToken", "TRT", 1000000000000);
+  //deployer.deploy(TrustReviewToken);
+
 };
